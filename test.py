0210d25dae484cb8dd527d95d69ce9168966020d
Zhiqiang Yan <74827816+yanzq95@users.noreply.github.com>
2024-02-22 14:00:08 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,142 @@
+import argparse
+import os
+
+from utils import *
+import numpy as np
+import torchvision.transforms as transforms
+from torchvision import utils
+from torch import Tensor
+from PIL import Image
+from torchvision.utils import save_image, make_grid
+
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+
+from model.SPFNet import *
+from data.nyu_dataloader import *
+from data.rgbdd_dataloader import *
+from data.middlebury_dataloader import Middlebury_dataset
+
+import cv2
+import os
+import time
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--scale', type=int, default=4, help='scale factor')
+parser.add_argument("--num_feats", type=int, default=42, help="channel number of the middle hidden layer")
+parser.add_argument("--root_dir", type=str, default='/opt/data/private/dataset/RGB-D-D', help="root dir of dataset")
+parser.add_argument("--model_dir", type=str, default="/opt/data/private/SPFNet/CKPT/SPFNet_Real.pth", help="path of model")
+parser.add_argument("--results_dir", type=str, default='/opt/data/private/SPFNet/results/', help="root dir of results")
+parser.add_argument('--tiny_model', action='store_true', help='tiny model')
+parser.add_argument("--downsample", type=str, default='sync', help="sync or real")
+
+opt = parser.parse_args()
+
+net = SPFNet(num_feats=opt.num_feats, kernel_size=3, scale=opt.scale, reduction=4, tiny_model=opt.tiny_model).cuda()
+net.load_state_dict(torch.load(opt.model_dir, map_location='cuda:0'))
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+net.to(device)
+
+data_transform = transforms.Compose([transforms.ToTensor()])
+
+dataset_name = opt.root_dir.split('/')[-1]
+if dataset_name == 'NYU-v2':
+    dataset = NYU_v2_datset(root_dir=opt.root_dir, scale=opt.scale, transform=data_transform, train=False)
+    test_minmax = np.load('%s/test_minmax.npy' % opt.root_dir)
+    rmse = np.zeros(449)
+elif dataset_name == 'RGB-D-D':
+    dataset = RGBDD_Dataset(root_dir=opt.root_dir, scale=opt.scale, downsample=opt.downsample, train=False, transform=data_transform)
+    rmse = np.zeros(405)
+elif dataset_name == 'Middlebury':
+    dataset = Middlebury_dataset(root_dir=opt.root_dir, scale=opt.scale, transform=data_transform)
+    rmse = np.zeros(30)
+elif dataset_name == 'Lu':
+    dataset = Middlebury_dataset(root_dir=opt.root_dir, scale=opt.scale, transform=data_transform)
+    rmse = np.zeros(6)
+
+dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=8)
+data_num = len(dataloader)
+
+with torch.no_grad():
+    net.eval()
+    if dataset_name == 'NYU-v2':
+        t = np.zeros(449)
+        for idx, data in enumerate(dataloader):
+            guidance, lr, gt, seg, ns = data['guidance'].cuda(), data['lr'].cuda(), data['gt'].cuda(), data[
+                'seg'].cuda(), data['ns'].cuda()
+            out = net((guidance, lr, seg, ns))
+            
+            minmax = test_minmax[:, idx]
+            minmax = torch.from_numpy(minmax).cuda()
+            rmse[idx] = calc_rmse(gt[0, 0], out[0, 0], minmax)
+            
+            path_output = '{}/output'.format(opt.results_dir)
+            os.makedirs(path_output, exist_ok=True)
+            path_save_pred = '{}/{:010d}.png'.format(path_output, idx)
+            
+            # Save results  (Save the output depth map)
+            pred = out[0,0] * (minmax[0] - minmax[1]) + minmax[1]
+            pred = pred * 1000.0
+            pred = pred.cpu().detach().numpy()
+            pred = pred.astype(np.uint16)
+            pred = Image.fromarray(pred)
+            pred.save(path_save_pred)
+            
+            # visualization  (Visual depth map)
+            #pred = out[0, 0]
+            #pred = pred.cpu().detach().numpy()
+            #cv2.imwrite(path_save_pred, pred * 255.0)   
+            
+            print(rmse[idx])
+        print(rmse.mean())
+        
+    elif dataset_name == 'RGB-D-D':
+        for idx, data in enumerate(dataloader):
+            guidance, lr, gt, seg, ns,maxx,minn = data['guidance'].to(device), data['lr'].to(device), data['gt'].to(device), data[
+                'seg'].to(device), data['ns'].to(device),data['max'].to(device),data['min'].to(device).to(device)
+            out = net((guidance, lr, seg,ns))
+            rmse[idx] = rgbdd_calc_rmse_sync(gt[0,0], out[0,0],[maxx,minn])
+            
+            path_output = '{}/output'.format(opt.results_dir)
+            os.makedirs(path_output, exist_ok=True)
+            path_save_pred = '{}/{:010d}.png'.format(path_output, idx)
+            
+            # Save results  (Save the output depth map)
+            pred = out[0, 0] * (maxx - minn) + minn
+            pred = pred.cpu().detach().numpy()
+            pred = pred.astype(np.uint16)
+            pred = Image.fromarray(pred)
+            pred.save(path_save_pred)
+            
+            # visualization  (Visual depth map)
+            #pred = out[0, 0]
+            #pred = pred.cpu().detach().numpy()
+            #cv2.imwrite(path_save_pred, pred * 255.0)   
+            print(rmse[idx])
+        print(rmse.mean())
+    elif (dataset_name == 'Middlebury') or (dataset_name == 'Lu'):
+        for idx, data in enumerate(dataloader):
+            guidance, lr, gt, seg, ns, maxx, minn = data['guidance'].to(device), data['lr'].to(device), data['gt'].to(device), data[
+                'seg'].to(device), data['ns'].to(device), data['max'].to(device), data['min'].to(device)
+            out = net((guidance, lr, seg,ns))
+            rmse[idx] = midd_calc_rmse(gt[0,0], out[0,0],[maxx,minn])
+            
+            path_output = '{}/output'.format(opt.results_dir)
+            os.makedirs(path_output, exist_ok=True)
+            path_save_pred = '{}/{:010d}.png'.format(path_output, idx)
+            
+            # Save results  (Save the output depth map)
+            pred = out[0,0] * 255.0
+            pred = pred.cpu().detach().numpy()
+            pred = pred.astype(np.uint16)
+            pred = Image.fromarray(pred)
+            pred.save(path_save_pred)
+
+            print(rmse[idx])
+        print(rmse.mean())
+
+
---
+++ train.py
@@ -0,0 +1,185 @@
+import argparse
+from model.SPFNet import *
+from model.common import *
+
+from data.nyu_dataloader import *
+from data.rgbdd_dataloader import *
+from data.middlebury_dataloader import Middlebury_dataset
+from utils import calc_rmse, rgbdd_calc_rmse, midd_calc_rmse
+
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+import torch.optim as optim
+import torch.nn as nn
+import torch.optim.lr_scheduler as lr_scheduler
+from tqdm import tqdm
+import logging
+from datetime import datetime
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--scale', type=int, default=4, help='scale factor')
+parser.add_argument('--lr',  default='0.0001', type=float, help='learning rate')
+parser.add_argument('--result',  default='experiment', help='learning rate')
+parser.add_argument('--epoch',  default=200, type=int, help='max epoch')
+parser.add_argument('--device',  default="1", type=str, help='which gpu use')
+parser.add_argument("--decay_iterations", type=list, default=[5e4, 1e5, 2e5], help="steps to start lr decay")
+parser.add_argument("--num_feats", type=int, default=42, help="channel number of the middle hidden layer")
+parser.add_argument("--gamma", type=float, default=0.2, help="decay rate of learning rate")
+parser.add_argument("--root_dir", type=str, default='/opt/data/private/dataset/NYU-v2', help="root dir of dataset")
+parser.add_argument("--batchsize", type=int, default=1, help="batchsize of training dataloader")
+parser.add_argument('--tiny_model', action='store_true', help='tiny model')
+
+opt = parser.parse_args()
+print(opt)
+os.environ["CUDA_VISIBLE_DEVICES"] = opt.device
+
+s = datetime.now().strftime('%Y%m%d%H%M%S')
+dataset_name = opt.root_dir.split('/')[-1]
+result_root = '%s/%s-lr_%s-s_%s-%s-b_%s' % (opt.result, s, opt.lr, opt.scale, dataset_name, opt.batchsize)
+if not os.path.exists(result_root):
+    os.mkdir(result_root)
+
+logging.basicConfig(filename='%s/train.log' % result_root, format='%(asctime)s %(message)s', level=logging.INFO)
+logging.info(opt)
+
+net = SPFNet(num_feats=opt.num_feats, kernel_size=3, scale=opt.scale, reduction=4, tiny_model=opt.tiny_model).cuda()
+criterion = nn.L1Loss()
+optimizer = optim.Adam(net.parameters(), lr=opt.lr)
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=opt.decay_iterations, gamma=opt.gamma)
+net.train()
+
+data_transform = transforms.Compose([transforms.ToTensor()])
+up = nn.Upsample(scale_factor=opt.scale, mode='bicubic')
+
+if dataset_name == 'NYU-v2':
+    test_minmax = np.load('%s/test_minmax.npy' % opt.root_dir)
+    train_dataset = NYU_v2_datset(root_dir=opt.root_dir, scale=opt.scale, transform=data_transform, train=True)
+    test_dataset = NYU_v2_datset(root_dir=opt.root_dir, scale=opt.scale, transform=data_transform, train=False)
+if dataset_name == 'RGB-D-D':
+    train_dataset = RGBDD_Dataset(root_dir=opt.root_dir, scale=opt.scale, downsample='real', train=True, transform=data_transform)
+    test_dataset = RGBDD_Dataset(root_dir=opt.root_dir, scale=opt.scale, downsample='real', train=False, transform=data_transform)
+
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=opt.batchsize, shuffle=True, num_workers=8)
+test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=8)
+
+max_epoch = opt.epoch
+num_train = len(train_dataloader)
+best_rmse = 10.0
+best_epoch = 0
+for epoch in range(max_epoch):
+    # ---------
+    # Training
+    # ---------
+    net.train()
+    running_loss = 0.0
+
+    t = tqdm(iter(train_dataloader), leave=True, total=len(train_dataloader))
+
+    for idx, data in enumerate(t):
+        batches_done = num_train * epoch + idx
+        optimizer.zero_grad()
+        guidance, lr, gt, seg, ns = data['guidance'].cuda(), data['lr'].cuda(), data['gt'].cuda(), data[
+            'seg'].cuda(), data['ns'].cuda()
+
+        out = net((guidance, lr, seg,ns))
+
+        loss = criterion(out, gt)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        running_loss += loss.data.item()
+        running_loss_50 = running_loss
+
+        if idx % 50 == 0:
+            running_loss_50 /= 50
+            t.set_description('[train epoch:%d] loss: %.8f' % (epoch + 1, running_loss_50))
+            t.refresh()
+
+    logging.info('epoch:%d iteration:%d running_loss:%.10f' % (epoch + 1, batches_done + 1, running_loss / num_train))
+
+    # -----------
+    # Validating
+    # -----------
+    if (epoch % 2 == 0) and (epoch < 30):
+        with torch.no_grad():
+
+            net.eval()
+            if dataset_name == 'NYU-v2_Our':
+                rmse = np.zeros(449)
+            if dataset_name == 'RGB-D-D':
+                rmse = np.zeros(405)
+            t = tqdm(iter(test_dataloader), leave=True, total=len(test_dataloader))
+
+            for idx, data in enumerate(t):
+                if dataset_name == 'NYU-v2_Our':
+                    guidance, lr, gt, seg, ns = data['guidance'].cuda(), data['lr'].cuda(), data['gt'].cuda(), data[
+                        'seg'].cuda(), data['ns'].cuda()
+
+                    out = net((guidance, lr, seg, ns))
+                    minmax = test_minmax[:, idx]
+                    minmax = torch.from_numpy(minmax).cuda()
+                    rmse[idx] = calc_rmse(gt[0, 0], out[0, 0], minmax)
+                if dataset_name == 'RGB-D-D':
+                    guidance, lr, gt, seg, ns, max, min = data['guidance'].cuda(), data['lr'].cuda(), data['gt'].cuda(), data['seg'].cuda(), data['ns'].cuda(), data[
+                        'max'].cuda(), data['min'].cuda()
+                    out = net((guidance, lr, seg, ns))
+                    minmax = [max, min]
+                    rmse[idx] = rgbdd_calc_rmse(gt[0, 0], out[0, 0], minmax)
+
+                    t.set_description('[validate] rmse: %f' % rmse[:idx + 1].mean())
+                    t.refresh()
+            r_mean = rmse.mean()
+            if r_mean < best_rmse:
+                best_rmse = r_mean
+                best_epoch = epoch
+                torch.save(net.state_dict(),
+                           os.path.join(result_root, "modelbest%f_8%d.pth" % (best_rmse, best_epoch + 1)))
+            logging.info(
+                '---------------------------------------------------------------------------------------------------------------------------')
+            logging.info('epoch:%d lr:%f-------mean_rmse:%f (BEST: %f @epoch%d)' % (
+                epoch + 1, scheduler.get_last_lr()[0], r_mean, best_rmse, best_epoch + 1))
+            logging.info(
+                '---------------------------------------------------------------------------------------------------------------------------')
+    elif epoch >= 30:
+        with torch.no_grad():
+
+            net.eval()
+            if dataset_name == 'NYU-v2_Our':
+                rmse = np.zeros(449)
+            if dataset_name == 'RGB-D-D':
+                rmse = np.zeros(405)
+            t = tqdm(iter(test_dataloader), leave=True, total=len(test_dataloader))
+
+            for idx, data in enumerate(t):
+                if dataset_name == 'NYU-v2_Our':
+                    guidance, lr, gt, seg, ns = data['guidance'].cuda(), data['lr'].cuda(), data['gt'].cuda(), data[
+                        'seg'].cuda(), data['ns'].cuda()
+
+                    out = net((guidance, lr, seg, ns))
+                    minmax = test_minmax[:, idx]
+                    minmax = torch.from_numpy(minmax).cuda()
+                    rmse[idx] = calc_rmse(gt[0, 0], out[0, 0], minmax)
+                if dataset_name == 'RGB-D-D':
+                    guidance, lr, gt, seg, ns, max, min = data['guidance'].cuda(), data['lr'].cuda(), data['gt'].cuda(), data['seg'].cuda(), data['ns'].cuda(), data[
+                        'max'].cuda(), data['min'].cuda()
+                    out = net((guidance, lr, seg, ns))
+                    minmax = [max, min]
+                    rmse[idx] = rgbdd_calc_rmse(gt[0, 0], out[0, 0], minmax)
+
+                    t.set_description('[validate] rmse: %f' % rmse[:idx + 1].mean())
+                    t.refresh()
+            r_mean = rmse.mean()
+            if r_mean < best_rmse:
+                best_rmse = r_mean
+                best_epoch = epoch
+            torch.save(net.state_dict(),
+                       os.path.join(result_root, "modelRmse%f_8%d.pth" % (r_mean, epoch + 1)))
+            logging.info(
+                '---------------------------------------------------------------------------------------------------------------------------')
+            logging.info('epoch:%d lr:%f-------mean_rmse:%f (BEST: %f @epoch%d)' % (
+                epoch + 1, scheduler.get_last_lr()[0], r_mean, best_rmse, best_epoch + 1))
+            logging.info(
+                '---------------------------------------------------------------------------------------------------------------------------')
+
