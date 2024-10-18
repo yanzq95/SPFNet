6ad6edb527c18ea3ee6332d13a6dfc7c6fbada55
Zhiqiang Yan <74827816+yanzq95@users.noreply.github.com>
2024-10-18 11:07:52 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,23 +1,22 @@
 import torch
 
+
 def calc_rmse(a, b, minmax):
-    # minmax = torch.from_numpy(minmax).cuda()
     a = a[6:-6, 6:-6]
     b = b[6:-6, 6:-6]
-    
-    a = a*(minmax[0]-minmax[1]) + minmax[1]
-    b = b*(minmax[0]-minmax[1]) + minmax[1]
+
+    a = a * (minmax[0] - minmax[1]) + minmax[1]
+    b = b * (minmax[0] - minmax[1]) + minmax[1]
     a = a * 100
     b = b * 100
-    
-    return torch.sqrt(torch.mean(torch.pow(a-b,2)))
-    
+
+    return torch.sqrt(torch.mean(torch.pow(a - b, 2)))
+
+
 def rgbdd_calc_rmse(gt, out, minmax):
-    # minmax = torch.from_numpy(minmax).cuda()
     gt = gt[6:-6, 6:-6]
     out = out[6:-6, 6:-6]
 
-    gt = gt*(minmax[0]-minmax[1]) + minmax[1]
     out = out * (minmax[0] - minmax[1]) + minmax[1]
     gt = gt / 10.0
     out = out / 10.0
@@ -28,10 +27,23 @@ def rgbdd_calc_rmse(gt, out, minmax):
 def midd_calc_rmse(gt, out, minmax):
     gt = gt[6:-6, 6:-6]
     out = out[6:-6, 6:-6]
-    # gt = gt * 255.0
-    # out = out * 255.0
 
     gt = gt * (minmax[0] - minmax[1]) + minmax[1]
     out = out * (minmax[0] - minmax[1]) + minmax[1]
 
-    return torch.sqrt(torch.mean(torch.pow(gt - out, 2)))  # rmse
+    return torch.sqrt(torch.mean(torch.pow(gt - out, 2)))
+
+
+def tofdsr_calc_rmse(gt, out, minmax):
+    gt = gt[6:-6, 6:-6]
+    out = out[6:-6, 6:-6]
+
+    mask = (gt >= 100) & (gt <= 5000)
+    gt = gt[mask]
+    out = out[mask]
+
+    out = out * (minmax[0] - minmax[1]) + minmax[1]
+    gt = gt / 10.0
+    out = out / 10.0
+
+    return torch.sqrt(torch.mean(torch.pow(gt - out, 2)))
